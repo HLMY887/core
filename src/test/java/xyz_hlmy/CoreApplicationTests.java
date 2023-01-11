ca212b647365b20707293b16e82fad6701a9515b
hlmy <yodc887@gmail>
2023-01-11 17:10:43 +0800

    one

--- src/test/java/xyz_hlmy/CoreApplicationTests.java
+++ src/test/java/xyz_hlmy/CoreApplicationTests.java
@@ -2,12 +2,32 @@ package xyz_hlmy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import xyz_hlmy.util.QRCodeUtil;
 
 @SpringBootTest
 class CoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        String analyzeImage = "F:\\123.png";
+        try {
+            String decode = QRCodeUtil.decode(analyzeImage);
+            System.out.println(decode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String text = "刘梓阳是傻逼";
+        String imag = "F:\\info.gif";
+        String destPath = "F:\\202301111634.png";
+        try {
+            QRCodeUtil.encode(text, imag,destPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
