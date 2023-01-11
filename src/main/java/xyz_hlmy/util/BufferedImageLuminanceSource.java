427a6b7ec7e7754d27fcc85648ccbffee75b049c
hlmy <yodc887@gmail>
2023-01-11 16:39:43 +0800

    Initial commit

---
+++ src/main/java/xyz_hlmy/util/BufferedImageLuminanceSource.java
@@ -0,0 +1,8 @@
+package xyz_hlmy.util;/**
+ * @ProjectName:    core 
+ * @ClassName:      BufferedImageLuminanceSource
+ * @Author:     lipenghui
+ * @Description:  图像亮度源  
+ * @Date:    2023/01/11 16:01
+ */public class BufferedImageLuminanceSource {
+}
---
+++ src/main/java/xyz_hlmy/util/QRCodeUtil.java
@@ -0,0 +1,8 @@
+package xyz_hlmy.util;/**
+ * @ProjectName:    core 
+ * @ClassName:      QRCodeUtil
+ * @Author:     lipenghui
+ * @Description:  二维码工具类  
+ * @Date:    2023/01/11 15:47
+ */public class QRCodeUtil {
+}
